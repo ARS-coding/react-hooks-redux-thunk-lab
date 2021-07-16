7f010180773b81c95371b89422d30c84788c858b
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-17 00:32:28 +0300

    Fix the typo 'images' to entities.

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function Cats() {
-  const arrayOfImagesObject = useSelector(state => state.images);
+  const arrayOfImagesObject = useSelector(state => state.entities);
+  console.log(arrayOfImagesObject)
   return (
     <div>
       <h1>CatBook</h1>
