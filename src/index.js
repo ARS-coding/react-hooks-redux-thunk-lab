282449aa0d9974f818ce68508c1f44308e749244
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-17 00:03:49 +0300

    Configure Redux store without a thunk.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import catsReducer from "./features/cats/catsSlice";
+
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(catsReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,document.getElementById("root")
+);
