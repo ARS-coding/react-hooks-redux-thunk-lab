5882534fb2e2494b7ba2db9d5b5303c688785206
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-17 00:16:50 +0300

    Set up the reducer.

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,23 @@
 export function fetchCats() {}
 
-export default function catsReducer() {}
+const initialState = {
+    entities: [],
+    status: "idle"
+};
+
+export default function catsReducer(state, action) {
+    switch(action.type) {
+        case "cats/catsLoading": // just pass an aciton with the corresponding type 
+            return {
+                ...state,
+                status: "loading"
+            };
+        
+        case "cats/catsLoaded": // just pass the array of cat objects as payload
+            return {
+                ...state,
+                entities: action.payload,
+                status: "idle"
+            };
+    };
+};
